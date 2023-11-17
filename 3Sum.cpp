dc627050b7acac5ce52e6ae112febf183413705e
hoavu-cs <59130321+hoavu-cs@users.noreply.github.com>
2023-11-17 15:29:57 -0800

    Update 3Sum.cpp

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -9,17 +9,17 @@ public:
 
         vector<vector<int>> res;
         set<tuple<int, int, int>> triple_set;
-
         sort(nums.begin(), nums.end());
-        vector<int> nums_deduplicated = {nums[0], nums[1], nums[2]};
-        
-        for (int i = 3; i < nums.size(); i++){
-            if (nums[i] != nums[i-3]) {
-                nums_deduplicated.push_back(nums[i]);
-            }
-        }
 
-        nums = nums_deduplicated;
+        if (nums.size() > 3) {
+            vector<int> nums_deduplicated = {nums[0], nums[1], nums[2]};
+            for (int i = 3; i < nums.size(); i++){
+                if (nums[i] != nums[i-3]) {
+                    nums_deduplicated.push_back(nums[i]);
+                }
+            }
+            nums = nums_deduplicated;
+        }
 
         for (int i = 0; i < nums.size() - 2; i++) {
             for (int j = i + 1; j < nums.size() - 1; j++){
