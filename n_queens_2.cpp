bdcb0af2956da61b14972c920b8bb83d778cc32c
hoavu-cs <59130321+hoavu-cs@users.noreply.github.com>
2023-12-02 22:08:58 -0800

    Create n_queens_2.cpp

---
+++ n_queens_2.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+
+class Solution {
+private:
+    int counter = 0;
+
+public:
+
+    bool is_valid(const vector<string>& board, int row, int col) {
+        int n = board.size();
+
+        // Check the column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check the upper left diagonal
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check the upper right diagonal
+        for (int i = row, j = col; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void solve(vector<string>& board, int row) {
+        int n = board.size();
+        
+        for(int col = 0; col < n; col++) {
+            if (is_valid(board, row, col)) {
+                board[row][col] = 'Q';
+                if (row < n - 1) { 
+                    solve(board, row + 1);
+                } else {
+                    counter++;
+                }
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    int totalNQueens(int n) {
+        vector<string> board  (n, string(n, '.'));
+        solve(board, 0);
+        return counter;
+    }
+};
